7b3cfa9918f0e27e65eec3e631f66b0983a354d0
Alexandre S <alexsdnwcz@gmail.com>
2025-02-27 17:13:05 +0100

    add search service and model

---
+++ swifty_companion/lib/models/usersearch_model.dart
@@ -0,0 +1,10 @@
+
+class UserSearch {
+  String login;
+  String avatar; //tmp
+
+  UserSearch({
+    required this.login,
+    required this.avatar
+  });
+}
---
+++ swifty_companion/lib/services/search_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:swifty_companion/models/usersearch_model.dart';
+
+class SearchService {
+  Future<List<UserSearch>> searchUsers(String query, String accessToken) async {
+    final Uri searchUri = Uri.parse("https://api.intra.42.fr/v2/users?range[login]=$query,${query}z");
+    final response = await http.get(
+      searchUri,
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+
+      List<UserSearch> users = data.map<UserSearch>((userData) {
+        return UserSearch(
+          login: userData["login"],
+          avatar: userData["image"]["link"] ?? '',
+        );
+      }).toList();
+
+      return users;
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+}
